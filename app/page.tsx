c1fe886ccdf120bee93c93982409043d5bc8288f
Manuel Pastor <pastorringuelet@gmail.com>
2023-11-15 21:34:53 +0100

    Add Prettier and prettier-plugin-tailwindcss, remove unused files and update page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,6 @@
-import { Button } from "@/components/ui/button";
 import { getUserAuth } from "@/lib/auth/utils";
-import Link from "next/link";
 
 export default async function Home() {
   const userAuth = await getUserAuth();
-  return (
-    <main className="space-y-6">
-      <Link href="/account">
-        <Button variant="outline">Account and Billing</Button>
-      </Link>
-      <pre className="bg-slate-100 dark:bg-slate-800 p-4">
-        {JSON.stringify(userAuth, null, 2)}
-      </pre>
-    </main>
-  );
+  return <main>Hello Trello</main>;
 }
